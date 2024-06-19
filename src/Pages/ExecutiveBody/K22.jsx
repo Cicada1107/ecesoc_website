237bf56baa32bbe91e64a17e5322afa5899b0fd7
Keshav Agarwal <64908981+MasterK0927@users.noreply.github.com>
2024-06-19 09:38:31 +0530

    Update K22.jsx
    Fixed image issue and resturctured the data.

--- src/Pages/ExecutiveBody/K22.jsx
+++ src/Pages/ExecutiveBody/K22.jsx
@@ -6,7 +6,7 @@ const K22 = [
         batch: "K22",
         position: "General Secretary",
         
-        imageUrl: "AbhyudayPandey",
+        imageUrl: "../../../public/K22images/AbhyudayPandey.jpg",
         linkedinUrl: "https://www.linkedin.com/in/abhyuday-pandey-2302ad/",
         githubUrl: "",
         instaUrl: "https://www.instagram.com/ab_hue_day.py/"
@@ -16,7 +16,7 @@ const K22 = [
         name: "Sahil Kumar",
         batch: "K22",
         position: "Joint secretary",
-        imageUrl: "SahilKumar",
+        imageUrl: "../../../public/K22images/SahilKumar.jpg",
         linkedinUrl: "https://www.linkedin.com/in/sahil-kumar-641220267?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         githubUrl: "",
         instaUrl: "https://www.instagram.com/notreallysahil?igsh=MWRyaTM3d2NveDBkcQ=="
@@ -26,17 +26,37 @@ const K22 = [
         name: "Harsh Ranjan",
         batch: "K22",
         position: "Joint secretary",
-        imageUrl: "HarshRanjan",
+        imageUrl: "../../../public/K22images/HarshRanjan.jpg",
         linkedinUrl: "https://www.linkedin.com/in/harsh-ranjan-65a16528a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         githubUrl: "",
         instaUrl: "https://www.instagram.com/_har_sh18?igsh=MTNtMGt4aG5jcHFsbQ=="
+    },
+    {
+        timestamp: "16/06/2024 18:02:35",
+        name: "Sushant Gupta",
+        batch: "K22",
+        position: "Treasurer",
+        imageUrl: "../../../public/K22images/SushantGupta.jpg",
+        linkedinUrl: "https://www.linkedin.com/in/sushant-gupta-0aab48255/",
+        githubUrl: "https://github.com/Sushant626",
+        instaUrl: "https://www.instagram.com/sushantgupta.626/"
+    },
+    {
+        timestamp: "16/06/2024 20:55:26",
+        name: "Prashant kumar Gautam",
+        batch: "K22",
+        position: "Treasurer",
+        imageUrl: "../../../public/K22imagesPrashantkumarGautam.jpg",
+        linkedinUrl: "https://www.linkedin.com/in/prashant-gautam-b84532291",
+        githubUrl: "",
+        instaUrl: "https://www.instagram.com/_prashant_725?igsh=empnYjBrOTFpNXY3"
     },
     {
         timestamp: "16/06/2024 23:53:09",
         name: "Keshav Agarwal",
         batch: "K22",
         position: "Tech Lead (Software)",
-        imageUrl: "KeshavAgarwal",
+        imageUrl: "../../../public/K22images/KeshavAgarwal.jpg",
         linkedinUrl: "https://linkedin.com/in/keshav-agarwal-0927",
         githubUrl: "https://github.com/masterK0927",
         instaUrl: "https://instagram.com/MasterK0927"
@@ -46,7 +66,7 @@ const K22 = [
         name: "Ansh Aryan",
         batch: "K22",
         position: "Technical Head (Core)",
-        imageUrl: "AnshAryan",
+        imageUrl: "../../../public/K22images/AnshAryan.jpg",
         linkedinUrl: "https://www.linkedin.com/in/ansh-aryan-25ab971aa?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         githubUrl: "",
         instaUrl: ""
@@ -56,48 +76,27 @@ const K22 = [
         name: "Ritik Raj",
         batch: "K22",
         position: "Web Coordinator",
-        imageUrl: "RitikRaj",
+        imageUrl: "../../../public/K22images/RitikRaj.jpg",
         linkedinUrl: "https://www.linkedin.com/in/ritik-raj1875/",
         githubUrl: "https://github.com/CodeSmith18",
         instaUrl: "https://www.instagram.com/ritikraj_1875/"
     },
     {
-        timestamp: "16/06/2024 18:02:35",
-        name: "Sushant Gupta",
-        batch: "K22",
-        position: "Treasurer",
-        imageUrl: "SushantGupta",
-        linkedinUrl: "https://www.linkedin.com/in/sushant-gupta-0aab48255/",
-        githubUrl: "https://github.com/Sushant626",
-        instaUrl: "https://www.instagram.com/sushantgupta.626/"
-    },
-    
-    {
         timestamp: "16/06/2024 19:56:23",
         name: "Abhishek Kumar",
         batch: "K22",
         position: "Content head",
-        imageUrl: "AbhishekKumar",
+        imageUrl: "../../../public/K22images/AbhishekKumar.jpg",
         linkedinUrl: "https://www.linkedin.com/in/abhishek-kumar-068488264/",
         githubUrl: "",
         instaUrl: ""
-    },
-    {
-        timestamp: "16/06/2024 20:55:26",
-        name: "Prashant kumar Gautam",
-        batch: "K22",
-        position: "Treasurer",
-        imageUrl: "PrashantkumarGautam",
-        linkedinUrl: "https://www.linkedin.com/in/prashant-gautam-b84532291",
-        githubUrl: "",
-        instaUrl: "https://www.instagram.com/_prashant_725?igsh=empnYjBrOTFpNXY3"
-    },
+    }, 
     {
         timestamp: "16/06/2024 21:43:18",
         name: "MRIGANK RAJ",
         batch: "K22",
         position: "Events coordinator",
-        imageUrl: "MRIGANKRAJ",
+        imageUrl: "../../../public/K22images/MRIGANKRAJ.jpg",
         linkedinUrl: "https://www.linkedin.com/in/mrigank-raj-040a84310",
         githubUrl: "https://github.com/Mrigank217",
         instaUrl: ""
@@ -107,7 +106,7 @@ const K22 = [
         name: "ANAND KASHYAP",
         batch: "K22",
         position: "sponsorship head",
-        imageUrl: "ANANDKASHYAP",
+        imageUrl: "../../../public/K22images/ANANDKASHYAP.jpg",
         linkedinUrl: "https://www.linkedin.com/in/anand-kashyap-a26a08251/",
         githubUrl: "https://github.com/calmanand",
         instaUrl: "https://www.instagram.com/anandkashyap372/"
@@ -118,7 +117,7 @@ const K22 = [
         name: "Sagun patwari",
         batch: "K22",
         position: "Design head",
-        imageUrl: "Sagunpatwari",
+        imageUrl: "../../../public/K22images/Sagunpatwari.jpg",
         linkedinUrl: "linkedin.com/in/sagun-patwari",
         githubUrl: "",
         instaUrl: "https://www.instagram.com/shagun_patwari/"
@@ -128,7 +127,7 @@ const K22 = [
         name: "Khushi Choudhary",
         batch: "K22",
         position: "Content Head",
-        imageUrl: "KhushiChoudhary",
+        imageUrl: "../../../public/K22images/KhushiChoudhary.jpg",
         linkedinUrl: "https://www.linkedin.com/in/khushi-choudhary-a45078260?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         githubUrl: "",
         instaUrl: "https://www.instagram.com/invites/contact/?igsh=cdvzeh4c7nrz&utm_content=p9sppzx"
@@ -138,7 +137,7 @@ const K22 = [
         name: "Prem Kumar Lohani",
         batch: "K22",
         position: "Event Coordinator",
-        imageUrl: "PremKumarLohani",
+        imageUrl: "../../../public/K22images/PremKumarLohani.jpg",
         linkedinUrl: "https://www.linkedin.com/in/prem-kumar-lohani-560855253",
         githubUrl: "",
         instaUrl: "https://www.instagram.com/premkumarlohani?igsh=MXYxaXRibXhvZDQzdw=="
