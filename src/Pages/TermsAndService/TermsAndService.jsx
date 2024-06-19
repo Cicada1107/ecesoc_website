3638d2a3be50f2c467419efdd67148109d2799a6
MasterK0927 <btech10189.22@bitmesra.ac.in>
2024-06-19 12:08:27 +0530

    fixed the metadata, added blog landing

--- src/Pages/TermsAndService/TermsAndService.jsx
+++ src/Pages/TermsAndService/TermsAndService.jsx
@@ -1,28 +1,30 @@
-import React from "react";
-import "./TermsAndService.scss";
+import React from 'react';
+import './TermsAndService.scss';
 
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 // Particles js Configuration file
-import particles from "../../particlesjs-config";
+import particles from '../../particlesjs-config';
 
 function TermsAndService(props) {
-	useEffect(() => {
-		tsParticles.load("particles", particles);
-	});
-	return (
-		<>
-			<div className="tos">
-				<div className="tos__header padding" id="particles">
-					<h1 className="tos__header__heading">About Us</h1>
-				</div>
-				<div className="tos__body padding">
-					
-				</div>
-			</div>
-		</>
-	);
+  useEffect(() => {
+    tsParticles.load('particles', particles);
+  });
+  return (
+    <>
+      <div className="tos">
+        <div className="tos__header padding" id="particles">
+          <h1 className="tos__header__heading">Blogs</h1>
+        </div>
+        <div className="tos__body padding">
+          <div className="tos__body__landing">
+            Landing Soon! Be Elcectrifyingly Curious
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-import "./TermsAndService.scss";
+import './TermsAndService.scss';
 
 export default TermsAndService;
